49e6a080dcb3e4f8dd7b71b2365c27d6f6724b80
kanyenortheast <60489016+kanyenortheast@users.noreply.github.com>
2022-02-08 22:51:20 +1300

    Classwork Tuesday 8/2

---
+++ elliotmenzinesreallyreallyreallybad.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <!-- Bootstrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <div class="container-fluid">
+
+      <div class="row bg-primary">
+        <div class="col-xl-8 col-md-6 col-sm-12 text-white">
+          <h1>Heading here</h1>
+        </div>
+        <div class="col-xl-4 col-md-6 col-sm-12 text-white">
+          <p>Cool</p>
+
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-12 col-md-6 col-xl-2 ">
+          <p>gamer 1</p>
+        </div>
+        <div class="col-12 col-md col-xl-2">
+          <p>gamer 2</p>
+        </div>
+        <div class="col-12 col-md-6 col-xl-2">
+          <p>gamer 3</p>
+        </div>
+        <div class="col-12 col-md-6 col-xl-2">
+          <p>gamer 4</p>
+        </div>
+
+      <div class="col-12 bg-success">
+        <div class="text-center">
+          <p> Iraq</p>
+
+        </div>
+
+      </div>
+
+      </div>
+
+    </div>
+
+
+    <!-- Link to Javascript for bootcamp -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
